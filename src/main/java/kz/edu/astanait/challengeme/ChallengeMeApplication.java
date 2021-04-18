d16507039c2c86fcaf7333ce01c8d8e284366c15
Beibarys Zholmyrza <beibaryszholmyrza@gmail.com>
2021-04-18 13:43:33 +0600

    first commit

---
+++ src/main/java/kz/edu/astanait/challengeme/ChallengeMeApplication.java
@@ -0,0 +1,13 @@
+package kz.edu.astanait.challengeme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeMeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChallengeMeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kz/edu/astanait/challengeme/ChallengeMeApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.edu.astanait.challengeme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeMeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
