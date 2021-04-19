a264bab48ecd130f59929fee1fd75397bd9f0ce3
Beibarys Zholmyrza <beibaryszholmyrza@gmail.com>
2021-04-19 15:11:06 +0600

    added: CRUD for category and priority

--- src/main/java/kz/edu/astanait/challengeme/controller/PriorityController.java
+++ src/main/java/kz/edu/astanait/challengeme/controller/PriorityController.java
@@ -2,11 +2,13 @@ package kz.edu.astanait.challengeme.controller;
 
 import kz.edu.astanait.challengeme.entity.Priority;
 import kz.edu.astanait.challengeme.repository.PriorityRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @RestController
 // используем @RestController вместо обычного @Controller, чтобы все ответы сразу оборачивались в JSON
@@ -25,5 +27,71 @@ public class PriorityController {
     public List<Priority> test() {
         List<Priority> list = priorityRepository.findAll();
         return list; // JSON формат будет использоваться автоматически
+    }
+    
+    @PostMapping("/add")
+    public ResponseEntity<Priority> add(@RequestBody Priority priority){
+        // проверка на обязательные параметры
+        if (priority.getId() != null && priority.getId() != 0) {
+            // id создается автоматически в БД (autoincrement), поэтому его передавать не нужно, иначе может быть конфликт уникальности значения
+            return new ResponseEntity("redundant param: id MUST be null", HttpStatus.NOT_ACCEPTABLE);
+        }
+        // если передали пустое значение title
+        if (priority.getTitle() == null || priority.getTitle().trim().length() == 0) {
+            return new ResponseEntity("missed param: title", HttpStatus.NOT_ACCEPTABLE);
+        }
+        // если передали пустое значение color
+        if (priority.getColor() == null || priority.getColor().trim().length() == 0) {
+            return new ResponseEntity("missed param: color", HttpStatus.NOT_ACCEPTABLE);
+        }
+        // save работает как на добавление, так и на обновление
+        return ResponseEntity.ok(priorityRepository.save(priority));
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity update(@RequestBody Priority priority){
+        // проверка на обязательные параметры
+        if (priority.getId() == null || priority.getId() == 0) {
+            return new ResponseEntity("missed param: id", HttpStatus.NOT_ACCEPTABLE);
+        }
+        // если передали пустое значение title
+        if (priority.getTitle() == null || priority.getTitle().trim().length() == 0) {
+            return new ResponseEntity("missed param: title", HttpStatus.NOT_ACCEPTABLE);
+        }
+        // если передали пустое значение color
+        if (priority.getColor() == null || priority.getColor().trim().length() == 0) {
+            return new ResponseEntity("missed param: color", HttpStatus.NOT_ACCEPTABLE);
+        }
+        // save работает как на добавление, так и на обновление
+        return ResponseEntity.ok(priorityRepository.save(priority));
+    }
+
+    // параметр id передаются не в BODY запроса, а в самом URL
+    @GetMapping("/id/{id}")
+    public ResponseEntity<Priority> findById(@PathVariable Long id) {
+        Priority priority = null;
+        // можно обойтись и без try-catch, тогда будет возвращаться полная ошибка (stacktrace)
+        // здесь показан пример, как можно обрабатывать исключение и отправлять свой текст/статус
+        try{
+            priority = priorityRepository.findById(id).get();
+        }catch (NoSuchElementException e){ // если объект не будет найден
+            e.printStackTrace();
+            return new ResponseEntity("id="+id+" not found", HttpStatus.NOT_ACCEPTABLE);
+        }
+        return  ResponseEntity.ok(priority);
+    }
+
+    // параметр id передаются не в BODY запроса, а в самом URL
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity delete(@PathVariable Long id) {
+        // можно обойтись и без try-catch, тогда будет возвращаться полная ошибка (stacktrace)
+        // здесь показан пример, как можно обрабатывать исключение и отправлять свой текст/статус
+        try {
+            priorityRepository.deleteById(id);
+        }catch (EmptyResultDataAccessException e){
+            e.printStackTrace();
+            return new ResponseEntity("id="+id+" not found", HttpStatus.NOT_ACCEPTABLE);
+        }
+        return new ResponseEntity(HttpStatus.OK); // не возвращаем удаленный объект
     }
 }
